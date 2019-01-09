7d8757656d9d572b89b75a6e053680f65705fae1
Kishan Patel <kolipatelkishan786@gmail.com>
2019-01-09 20:05:58 +0530

    97 Injecting the Logging Service into Components.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,21 +1,24 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private logginservise: LoggingService) { }
   onSetTo(status: string){
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+   this.logginservise.logStatusChange(status);
   }
 
-  constructor() { }
+
 
   ngOnInit() {
   }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -4,23 +4,25 @@ import {LoggingService} from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.scss']
+  styleUrls: ['./new-account.component.scss'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  onCreateAccount(accountName: string, accountStatus: string){
+  constructor(private logginservice: LoggingService) {
+  }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+    this.logginservice.logStatusChange(accountStatus);
   }
-
-  constructor() { }
 
   ngOnInit() {
   }
+
 
 }
